8033e1870b474a993af93f888acd0a4cc4942623
Loi Pham <loitpham@gmail.com>
2021-06-27 19:19:22 -0700

    Sending and receiving Codable data with URLSession and SwiftUI.

--- Cupcakes/Cupcakes/ContentView.swift
+++ Cupcakes/Cupcakes/ContentView.swift
@@ -24,10 +24,52 @@ class User: ObservableObject, Codable {
     }
 }
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State var results = [Result]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=pet+shop+boys&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        results = decodedResponse.results
+                    }
+                    return
+                }
+            }
+            
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+            
+        }
+        .resume() // most often forgotten
     }
 }
 
