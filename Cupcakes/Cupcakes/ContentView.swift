725b0b420400b9baf6886d8d65dfc5736251219b
Loi Pham <loitpham@gmail.com>
2021-06-27 18:24:15 -0700

    Adding Codable conformance for @Published properties.

---
+++ Cupcakes/Cupcakes/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Cupcakes
+//
+//  Created by Loi Pham on 6/27/21.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    @Published var name = "Loi Pham"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cupcakes/Cupcakes/CupcakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakesApp.swift
+//  Cupcakes
+//
+//  Created by Loi Pham on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
